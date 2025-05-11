1963737ad1724e718ec366a869cf04c6eff4b2d1
Rishat <virisound@gmail.com>
2025-05-11 06:44:40 +0500

    Created: Components, Link

---
+++ components/TheFooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TheFooter = () => {
+    return (
+        <footer className='container'>
+            Created by &copy;Rishat 
+        </footer>
+    );
+};
+
+export default TheFooter;
