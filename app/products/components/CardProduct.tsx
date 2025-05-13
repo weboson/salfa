a2fd1b2e1834d122557ebbbec9481103c49c2ed0
Rishat <virisound@gmail.com>
2025-05-14 03:56:51 +0500

    Used dinamic Link

--- app/products/components/CardProduct.tsx
+++ app/products/components/CardProduct.tsx
@@ -2,6 +2,7 @@
 // client component: карточка товара
 import React from "react";
 import "../styles.css";
+import Link from "next/link";
 
 const CardProduct = ({ product }: any) => {
   return (
@@ -9,26 +10,25 @@ const CardProduct = ({ product }: any) => {
       <div className="card-wrapper">
         <div className="card">
           <div className="image-wrapper">
-            <img
-              src={product.image}
-              alt=""
-              className="image"
-            />
+            <Link href={`/products/${product.id}`}>
+              <img src={product.image} alt="" className="image" />
+            </Link>
           </div>
           <div className="content-wrapper">
             <div className="title">
               <h4>{product.title}</h4>
             </div>
             <div className="price">{product.price}</div>
-            <div className="description">
-{product.description}
-            </div>
+            <div className="description">{product.description}</div>
             <div className="actions">
               <button id="cart" className="button flip green-solid cart">
                 <span className="front">Favorite</span>
                 <span className="backside">Added</span>
               </button>
-              <button className="button black-simple more">More Details</button>
+              <Link href={`/products/${product.id}`}>
+                <img src={product.image} alt="" className="image" />
+                <button className="button black-simple more">Подробнее</button>
+              </Link>
             </div>
           </div>
         </div>
