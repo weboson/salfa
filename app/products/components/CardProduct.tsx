47d82fd4d63b9cdcea65ee0fcb6c2695a81e6774
Rishat <virisound@gmail.com>
2025-05-14 02:14:28 +0500

    Added  HTML - template, CSS - styles

--- app/products/components/CardProduct.tsx
+++ app/products/components/CardProduct.tsx
@@ -6,7 +6,33 @@ import "../styles.css";
 const CardProduct = ({ product }: any) => {
   return (
     <>
-      {product.title}
+      <div className="card-wrapper">
+        <div className="card">
+          <div className="image-wrapper">
+            <img
+              src={product.image}
+              alt=""
+              className="image"
+            />
+          </div>
+          <div className="content-wrapper">
+            <div className="title">
+              <h4>{product.title}</h4>
+            </div>
+            <div className="price">{product.price}</div>
+            <div className="description">
+{product.description}
+            </div>
+            <div className="actions">
+              <button id="cart" className="button flip green-solid cart">
+                <span className="front">Favorite</span>
+                <span className="backside">Added</span>
+              </button>
+              <button className="button black-simple more">More Details</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
